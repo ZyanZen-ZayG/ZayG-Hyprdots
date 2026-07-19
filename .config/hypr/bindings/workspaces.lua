a98a3aa4b4c11bec45b0e02868ac3d5bec669093
ZyanZen-ZayG <jeffkulas5890@gmail.com>
2026-07-20 02:21:24 +0600

    sync: all live configs - waybar, keybinds, vars, windows

--- .config/hypr/bindings/workspaces.lua
+++ .config/hypr/bindings/workspaces.lua
@@ -5,7 +5,7 @@ for ws = 1, 10 do
 end
 
 -- Scratchpad
-hl.bind("SUPER + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }), { description = "Move to Scratchpad" })
+hl.bind("SUPER + SHIFT + RETURN", hl.dsp.window.move({ workspace = "special:magic" }), { description = "Move to Scratchpad" })
 
 -- Scroll through workspaces
 hl.bind("SUPER + mouse_down", hl.dsp.focus({ workspace = "e+1" }), { description = "Next Workspace" })
